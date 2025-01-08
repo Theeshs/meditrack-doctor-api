0a5fae1435742402de7e191a2b0f23b6c9c92589
thee <theekshana.sandaru@gmail.com>
2025-01-09 01:01:20 +0800

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,14 @@
-echo "building new docker image"
-docker build -t doctor-service-api .
-
-echo "appliyng deployment yml"
-kubectl apply -f deployment.yaml
-
-echo "enabled minicube docker env"
-eval $(minikube docker-env)
-
 echo "removing unused docker containers"
 docker container prune -f
 
 echo "removing unused docker images"
 docker image prune -f
 
-echo "forwarding port"
-nohup kubectl port-forward svc/doctor-api-service 3001:3001 > port-forward.log 2>&1 &
+echo "enabled minicube docker env"
+eval $(minikube docker-env)
+
+echo "building new docker image"
+docker build -t doctor-service-api .
+
+echo "appliyng deployment yml"
+kubectl apply -f deployment.yaml
